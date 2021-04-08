509d81cf7515d7bbf9adac30b25a906e7eb35229
redrob101 <80754561+redrob101@users.noreply.github.com>
2021-04-08 15:33:08 +0200

    Update FTv2.pl

--- FTv2.pl
+++ FTv2.pl
@@ -2,6 +2,7 @@ use strict;
 use warnings;
 use v5.10;
 use Getopt::Long;
+use Net::Ping;
 
 my $transfer_type="scp";
 my $listfile = "";
@@ -52,6 +53,12 @@ sub TransferMultipleFilesSftp {
     close $FH;
 }
 
+sub check_avail { 
+    my $p = Net::Ping->new();
+    $p->ping($host) or die "Host is unreachable...";
+    $p->close();
+}
+
 GetOptions('t=s' => \ $transfer_type,
            'l=s' => \ $listfile,
            'k=s' => \ $key,
@@ -69,29 +76,37 @@ GetOptions('t=s' => \ $transfer_type,
     if ($key eq "" and $listfile eq "" and $transfer_type ne "sftp" and $user ne "" and $host ne "") { #no key and no list
         $command = "$transfer_type $file $user\@$host:$file";
         say "\ntransferring a single file using 'scp'\n";
+        &check_avail;
         system($command);
     } elsif ($key ne "" and $listfile eq "" and $transfer_type ne "sftp") { #key and no list
         $command = "$transfer_type -i $key $file $user\@$host:$file";
         say "\ntransferring a single file using 'scp' and the provided key \'$key\'\n";
+        &check_avail;
         system($command);
     } elsif ($listfile ne "" and $key eq "" and $transfer_type ne "sftp"){ #no key and list
+        &check_avail;
         &TransferMultipleFilesScp;
     } elsif ($listfile ne "" and $key ne "" and $transfer_type ne "sftp"){ #key and list
         $transfer_type = "scp -i";
+        &check_avail;
         &TransferMultipleFilesScp;
     } elsif($transfer_type eq "sftp" and $listfile eq "" and $key eq ""){ #sftp, no key and no list
         say "\ntransferring a single file using 'sftp'\n";
         $command = "$transfer_type $user\@$host $hd";
+        &check_avail;
         system ($command);
     } elsif($transfer_type eq "sftp" and $listfile eq "" and $key ne ""){ #sftp, key and no list
         say "\ntransferring a single file using 'sftp' and the provided key \'$key\'\n";
         $transfer_type = "sftp -i";
-        $command = "$transfer_type $key $user\@$host $hd"; 
+        $command = "$transfer_type $key $user\@$host $hd";
+        &check_avail;
         system ($command);
     } elsif ($transfer_type eq "sftp" and $listfile ne "" and $key eq ""){ #sftp, list and no key
+        &check_avail;
         TransferMultipleFilesSftp;
     } elsif ($transfer_type eq "sftp" and $listfile ne "" and $key ne "") { #sftp, list and key
         $transfer_type = "sftp -i";
+        &check_avail;
         TransferMultipleFilesSftp;
     } elsif ($file eq "" and $user eq "" and $host eq ""){
         say "you need to specify at least three parameters: -f -u -h\n Example: perl $0 -f <filename> -u <user> -h <host>";
